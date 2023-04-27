f0ce7ce3ba0f849af30c19735c50140d8edc566f
Matheus Saimon <matheus30120@gmail.com>
2023-04-27 18:25:57 -0300

    Arrumando CADASTRO

--- src/pages/cadastro.js
+++ src/pages/cadastro.js
@@ -5,7 +5,7 @@ export default  function Cadastro(){
         <><div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
             <h1 className="display-6">Cadastro de assinante</h1>
         </div>
-        <div class="form-group col-md-6 text-align-center">
+        <div className="pricing-header1 px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
                 <form class="needs-validation">
                     <h4>Dados pessoais</h4>
                     <label>
@@ -29,11 +29,27 @@ export default  function Cadastro(){
                     <button type="button" class="btn btn-secondary">Free</button>
                     <button type="button" class="btn btn-secondary">Plus</button>
                     </div>
-                    <br />
-                    <br />
-                    <button type="submit" class="btn btn-dark">Assinar</button>
+                    <br />      
                 </form>
-
+                <form class="needs-validation">
+                    <h4>Dados do cartão</h4>
+                    <label>
+                        Numero do cartão:
+                        <input type="number" placeholder='0000 0000 0000 0000'/>
+                    </label>
+                    <br />
+                    <label>
+                        Nome no cartão:
+                        <input type="text" />
+                    </label>
+                    <br />
+                    <label>
+                        CVC:
+                        <input type="number" placeholder='CVC'/>
+                    </label>
+                    <br />
+                </form>
+                <button type="submit" class="btn btn-dark">Assinar</button>
             </div></>
         
     )
